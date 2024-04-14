e7ceb55375d9eb07dd9376d7628ac398a36f079e
unknown <cvetkovicc.andrej@gmail.com>
2024-04-14 19:58:24 +0200

    Database created

---
+++ BazaPodataka/TakmicariZaKickBox.sql
@@ -0,0 +1,8 @@
+﻿create table Takmicari
+(
+	ID int not null identity(1,1) primary key,
+	Ime varchar(50) not null,
+	Prezime varchar(50) not null,
+	Visina varchar(50) not null,
+	Tezina int not null
+);
